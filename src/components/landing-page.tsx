8647d62304ed0f8c5c488ac45e12f78f72d03a5f
Cyberkid-30 <janphandoh@gmail.com>
2025-06-05 18:26:47 +0000

    changed to inter font

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -4,7 +4,7 @@ import IconItem from "./icons/iconItem";
 export default function LandingPage() {
   return (
     <div className="relative min-h-screen bg-car flex flex-col justify-center items-center text-white text-center px-6">
-      <h1 className="text-4xl md:text-7xl font-roboto font-semibold mb-10 animate-from-top">
+      <h1 className="text-4xl md:text-7xl font-inter font-semibold mb-10 animate-from-top">
         Welcome to The Velvet Green
       </h1>
       <div className="flex gap-5 flex-wrap justify-center">
