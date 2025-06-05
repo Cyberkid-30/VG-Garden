052135164d018b2ab9d7a901da70c359723500d4
Cyberkid-30 <janphandoh@gmail.com>
2025-06-05 18:14:04 +0000

    Changed font

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -4,8 +4,8 @@ import IconItem from "./icons/iconItem";
 export default function LandingPage() {
   return (
     <div className="relative min-h-screen bg-car flex flex-col justify-center items-center text-white text-center px-6">
-      <h1 className="text-4xl md:text-7xl font-handlee font-semibold mb-10 animate-from-top">
-        Welcome to Velvet Green Garden
+      <h1 className="text-4xl md:text-7xl font-roboto font-semibold mb-10 animate-from-top">
+        Welcome to The Velvet Green
       </h1>
       <div className="flex gap-5 flex-wrap justify-center">
         {Icons.map((icon) => (
